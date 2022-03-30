3205000f6e57e3f63c0fbe39aa1b0ffaf3b64d5f
Mark Höllmann <mark.hoellmann@dfki.de>
2022-03-30 16:53:22 +0200

    setup ci (docker build image)

--- .devcontainer/installDependencies.sh
+++ .devcontainer/installDependencies.sh
@@ -28,7 +28,8 @@ apt-get update -qq && apt-get install -y -qq --no-install-recommends \
     python3-pip \
     ruby shellcheck \
     clang-format-10 \
-    python3-catkin-lint \
+    python3-catkin-lint
+
 pip3 install --upgrade pip
 pip3 install pre-commit
 
@@ -38,53 +39,73 @@ echo '####################################################'
 bash <(curl -s https://apt.kitware.com/kitware-archive.sh)
 apt-get update -q && apt-get install -qy cmake
 
-echo '####################################################'
-echo '#install flatbuffer'
-echo '####################################################'
-wget https://github.com/google/flatbuffers/archive/refs/tags/v2.0.0.tar.gz
-tar -xf v2.0.0.tar.gz
-mkdir flatbuffers-2.0.0/build
-cd flatbuffers-2.0.0/build  || exit 1
-cmake ..
-make -j"$(nproc)"
-make install
+# echo '####################################################'
+# echo '#install flatbuffer'
+# echo '####################################################'
+# wget https://github.com/google/flatbuffers/archive/refs/tags/v2.0.0.tar.gz
+# tar -xf v2.0.0.tar.gz
+# mkdir flatbuffers-2.0.0/build
+# cd flatbuffers-2.0.0/build  || exit 1
+# cmake ..
+# make -j"$(nproc)"
+# make install
 
-cd ../..  || exit 1
-rm -rf flatbuffers-2.0.0 v2.0.0.tar.gz
+# cd ../..  || exit 1
+# rm -rf flatbuffers-2.0.0 v2.0.0.tar.gz
 
-echo '####################################################'
-echo '#install gRPC'
-echo '####################################################'
+# echo '####################################################'
+# echo '#install protobuf via precompiled binaries from github'
+# echo '####################################################'
+# PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+# PB_VER="3.15.6"
+# curl -LO $PB_REL/download/v$PB_VER/protobuf-cpp-$PB_VER.zip
+# unzip protobuf-cpp-$PB_VER.zip
+# cd protobuf-$PB_VER || exit 1
+# ./configure --prefix=/usr
 
-#non-local install dirs (in docker installed globally)
-#create install dir and add to path
-#export GRPC_INSTALL_DIR=/grpc
-#mkdir ${GRPC_INSTALL_DIR}
-#export PATH="$PATH:$GRPC_INSTALL_DIR/bin"
+# make -j"$(nproc)"
+# make check -j"$(nproc)"
+# make install
+# ldconfig # refresh shared library cache.
 
-echo '#clone the grpc repo'
-git clone --recurse-submodules --depth 1 -b v1.35.0 https://github.com/grpc/grpc
+# echo '#remove protobuf code'
+# cd .. || exit 1
+# rm protobuf-cpp-$PB_VER.zip
+# rm -rf protobuf-$PB_VER
 
-echo '#build and install gRPC and Protocol Buffers'
-cd grpc || exit 1
-mkdir -p cmake/build
-pushd cmake/build || exit 1
-cmake -DgRPC_INSTALL=ON \
-    -DgRPC_BUILD_TESTS=OFF \
-    ../..
-make -j"$(nproc)"
-make install
-popd || exit 1
+# echo '####################################################'
+# echo '#install gRPC'
+# echo '####################################################'
 
-echo '#remove gRPC code'
-cd .. || exit 1
-rm -rf grpc
+# #non-local install dirs (in docker installed globally)
+# #create install dir and add to path
+# #export GRPC_INSTALL_DIR=/grpc
+# #mkdir ${GRPC_INSTALL_DIR}
+# #export PATH="$PATH:$GRPC_INSTALL_DIR/bin"
 
-echo '#install python3 grpcio_tools'
-pip3 install grpcio-tools
+# echo '#clone the grpc repo'
+# git clone --recurse-submodules --depth 1 -b v1.35.0 https://github.com/grpc/grpc
 
-echo '#updating python proto stuff'
-pip3 install --upgrade protobuf
+# echo '#build and install gRPC and Protocol Buffers'
+# cd grpc || exit 1
+# mkdir -p cmake/build
+# pushd cmake/build || exit 1
+# cmake -DgRPC_INSTALL=ON \
+#     -DgRPC_BUILD_TESTS=OFF \
+#     ../..
+# make -j"$(nproc)"
+# make install
+# popd || exit 1
+
+# echo '#remove gRPC code'
+# cd .. || exit 1
+# rm -rf grpc
+
+# echo '#install python3 grpcio_tools'
+# pip3 install grpcio-tools
+
+# echo '#updating python proto stuff'
+# pip3 install --upgrade protobuf
 
 echo '####################################################'
 echo '#misc'
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # README
+
+## Quick Start
+
+### VS Code devcontainer
+
+1. Clone this repo and open it in VS Code.
+2. Install the extensions `ms-vscode-remote.remote-containers` and `ms-azuretools.vscode-docker`.
+3. Press `F1` or `CTRL + SHIFT + P` and enter `Remote-Containers: Reopen Folder in Container`
+4. This creates a docker container based on
+   [ghcr.io/naturerobots/hsos-sep-plant-map-2022:main](https://github.com/naturerobots/HSOS-SEP-PlantMap-2022/pkgs/container/hsos-sep-plant-map-2022),
+   installs all necessary VS Code extensions, builds the workspace a first time, sets up
+   Intellisense, installs the [pre-commit](#precommit) hooks and opens it in VS Code.
+
+   Use `pre-commit run -a` in the workspace folder to check the code style before commiting. In the Docker image the
+   pre-commit checks are installed in the git so a commit is just possible if the checks succeed.
+
+5. default user: `docker` with password: `docker`
+
+## pre-commit formatting checks
+
+This repo has a [pre-commit](https://pre-commit.com/) check that runs in CI. You can use this locally and set it up to
+run automatically before you commit something. In the devcontainer it is already pre-installed. To install, use pip:
+
+```bash
+pip3 install --user pre-commit
+```
+
+To run over all the files in the repo manually:
+
+```bash
+pre-commit run -a
+```
+
+To run pre-commit automatically before committing in the local repo, install the git hooks (run it in the repo folder):
+
+```bash
+pre-commit install
+```
