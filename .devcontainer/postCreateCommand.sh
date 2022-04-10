ea25cc1cefee3110e444e8c2c34e7e35eb0acd80
Julian Arkenau <julian.arkenau@dfki.de>
2022-04-10 19:32:15 +0000

    fix: postCreate script cp error

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 #copy vs code config to root
-cp -r /home/docker/workspace/src/.devcontainer/.vscode/* /home/docker/workspace/.vscode/
-
-#build the workspace
-#TODO
+cp -r /home/docker/workspace/src/.devcontainer/.vscode/* /home/docker/workspace/src/.vscode/
 
 #install pre-commit in the git repo
 cd /home/docker/workspace/src || exit 1
