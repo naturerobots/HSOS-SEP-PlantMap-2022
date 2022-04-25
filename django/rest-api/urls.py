0f87280f032f4bd1645afece51c7bb597727b729
Julian Arkenau <julian.arkenau@dfki.de>
2022-04-25 10:26:15 +0000

    feat: initial django setup

---
+++ django/rest-api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+
+from . import views
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', views.list_projects),
+    path('<str:uuid>', views.list_project_detials),
+]
---
+++ django/rest-api/views.py
@@ -0,0 +1,32 @@
+import string
+import sys
+
+# there is definitly a better way to add an import path
+sys.path.append(r'../build/gRPC/')
+
+import grpc
+import meta_operations_service_pb2_grpc as metaOperations
+from google.protobuf import empty_pb2
+from google.protobuf.json_format import MessageToDict
+from project_query_pb2 import ProjectQuery
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+SERVER_URL = "seerep.naturerobots.de:5000"
+channel = grpc.insecure_channel(SERVER_URL)
+
+stub = metaOperations.MetaOperationsStub(channel)
+
+
+@api_view(['GET'])
+def list_projects(request) -> Response:
+    if request.method == 'GET':
+        response = stub.GetProjects(empty_pb2.Empty())
+        return Response(MessageToDict(response))
+
+
+@api_view(['GET'])
+def list_project_detials(request, uuid: string) -> Response:
+    if request.method == 'GET':
+        response = stub.GetProjectDetails(ProjectQuery(projectuuid=uuid))
+        return Response(MessageToDict(response))
