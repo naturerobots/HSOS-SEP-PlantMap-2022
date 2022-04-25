bfeeb9bbe40f3853b307f2a98327991d44a2d632
Anafabula <57800226+Anafabula@users.noreply.github.com>
2022-04-25 17:02:16 +0000

    build: fix container build

--- .devcontainer/installDependencies.sh
+++ .devcontainer/installDependencies.sh
@@ -36,37 +36,15 @@ pip3 install grpcio grpcio-tools
 
 #####*FRONTEND*#####
 echo '####################################################'
-echo '#INSTALL FRONTEND'
+echo '#install npm'
 echo '####################################################'
-
-echo '### - npm - ###'
-apt-get update -q && apt-get install -qy npm
-
-echo '### - nodejs 14.x - ###'
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt install nodejs -y
+apt-get update -q && apt install nodejs -y
 
-echo '### - create-vue - ###'
+echo '####################################################'
+echo '#install create-vue'
+echo '####################################################'
 npm install -g create-vue
-
-echo '### - axios - ###'
-npm install --save axios
-npm install --save vue-axios
-
-echo '### - leaflet - ###'
-npm i leaflet
-npm i @types/leaflet
-
-echo '### - fontawesome - ###'
-npm i --save @fortawesome/fontawesome-svg-core
-npm i --save @fortawesome/free-solid-svg-icons
-npm i --save @fortawesome/vue-fontawesome@prerelease
-
-echo '### - tailwindcss - ###'
-npm i -D tailwindcss
-
-echo '### - daisyui - ###'
-npm i daisyui
 
 # remove apt lists so that they are not saved in the image layers
 rm -rf /var/lib/apt/lists/*
