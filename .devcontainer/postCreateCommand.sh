ecf15d5148b55c8dacfb6611d4aea80dcdac4550
Julian Arkenau <julian.arkenau@dfki.de>
2022-04-25 09:10:36 +0000

    build:  python classes from protobuf definitions at startup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,3 +6,10 @@ cp -r /home/docker/workspace/src/.devcontainer/.vscode/* /home/docker/workspace/
 #install pre-commit in the git repo
 cd /home/docker/workspace/src || exit 1
 pre-commit install --install-hooks
+
+#generate python classes from protobuf messages
+python3 -m grpc_tools.protoc \
+        -I=protobuf-msgs \
+        --python_out=build/gRPC  \
+        --grpc_python_out=build/gRPC \
+        protobuf-msgs/*
