71d3322723c679dea86d2cee02a8ed1b8532b0d7
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-05-03 18:51:36 +0000

    feat: add dashboard, crops view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import DashboardView from "../views/DashboardView.vue";
+import CropsView from "../views/CropsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      redirect: "/home",
+      redirect: "/dashboard",
     },
     {
-      path: "/home",
-      name: "home",
-      component: HomeView,
+      path: "/dashboard",
+      name: "dashboard",
+      component: DashboardView,
+    },
+    {
+      path: "/crops",
+      name: "crops",
+      component: CropsView,
     },
   ],
 });
