c27960707188605da4b0a2c0262c2f9019bd8064
lucas <lucas.siebels@hs-osnabrueck.de>
2022-05-04 17:50:04 +0000

    feat: simple string output of weather on dashboard

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,5 +1,22 @@
 <template>
   <h1>Dashboard</h1>
+
+  <!--Test for weather / can be removed-->
+  {{ weather }}
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//####################################
+//Test for weather / can be cleaned up
+import { Ref, onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import { weatherStore } from "../stores/weatherStore";
+import type { Weather } from "../types/weather";
+
+const weather: Ref<Weather> = storeToRefs(weatherStore()).getWeather;
+
+onMounted(() => {
+  weatherStore().loadDataFromApi();
+});
+//####################################
+</script>
