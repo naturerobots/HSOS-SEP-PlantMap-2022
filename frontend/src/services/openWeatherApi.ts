8b3fc30d8dff75e681369bd35bed90bd27726083
Lugg <57572111+lucaslootalot@users.noreply.github.com>
2022-05-06 13:40:41 +0000

    fix: lon and lat now have the right values

--- frontend/src/services/openWeatherApi.ts
+++ frontend/src/services/openWeatherApi.ts
@@ -2,33 +2,30 @@ import axios from "axios";
 import type { Weather, WeatherCurrent, WeatherForecast } from "@/types/weather";
 
 //TODO: Find a solution that the token is not directly implemented
-const token = "a8b46be95a90a7e303aa7edd4fc1463e";
+const appid = "a8b46be95a90a7e303aa7edd4fc1463e";
 const baseURL = "https://api.openweathermap.org/data/2.5";
 const exclude = "current,minutely,hourly";
+const lang = "de";
+const units = "metric";
 
 //TODO: maybe better naming for "weather"
-export async function getWeatherInformation(lon: number, lat: number) {
+export async function getWeatherInformation(lat: number, lon: number) {
   const weather: Weather = {
     current: {} as WeatherCurrent,
     forecast: [] as WeatherForecast[],
   };
 
-  //test lon + lat -> Osnabrueck
-  lon = 52.2799112;
-  lat = 8.0471788;
-
   //API call for the current weather data
-  //https://api.openweathermap.org/data/2.5/weather?lat=52.2799112&lon=8.0471788&lang=de&appid=a8b46be95a90a7e303aa7edd4fc1463e
-  //let response = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat=52.2799112&lon=8.0471788&lang=de&appid=a8b46be95a90a7e303aa7edd4fc1463e");
+  //https://api.openweathermap.org/data/2.5/weather?lat=52.2799112&lon=8.0471788&lang=de&units=metric&appid=a8b46be95a90a7e303aa7edd4fc1463e
   let response = await axios.get(baseURL + "/weather", {
-    params: { lat: lat, lon: lon, lang: "de", appid: token },
+    params: { lat, lon, lang, units, appid },
   });
   weather.current = createWeatherCurrentObj(response.data);
 
   //API call for the 7 days forecast data
-  //https://api.openweathermap.org/data/2.5/onecall?lat=52.2799112&lon=8.0471788&exclude=minutely,hourly&lang=de&appid=a8b46be95a90a7e303aa7edd4fc1463e
+  //https://api.openweathermap.org/data/2.5/onecall?lat=52.2799112&lon=8.0471788&exclude=minutely,hourly&lang=de&units=metric&appid=a8b46be95a90a7e303aa7edd4fc1463e
   response = await axios.get(baseURL + "/onecall", {
-    params: { lat: lat, lon: lon, exclude: exclude, lang: "de", appid: token },
+    params: { lat, lon, exclude, lang, units, appid },
   });
   weather.forecast = createWeatherForecastObj(response.data);
 
