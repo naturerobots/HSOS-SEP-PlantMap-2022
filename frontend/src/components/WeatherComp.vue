ea90d18292db10643065ab739f96651ba17c74b9
Lugg <57572111+lucaslootalot@users.noreply.github.com>
2022-05-07 17:59:49 +0000

    feat: todo added for date formatting

--- frontend/src/components/WeatherComp.vue
+++ frontend/src/components/WeatherComp.vue
@@ -1,6 +1,8 @@
 <!---
 TODO: If no weather information is available because the API is not accessible or the location cannot be found,
 the component should be extended to include a message indicating that no weather data is available.
+
+TODO: date needs to be formatted and added -> https://coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript
 -->
 
 <template>
@@ -12,12 +14,18 @@ the component should be extended to include a message indicating that no weather
       <div class="card card-side">
         <!-- <font-awesome-icon icon="fa-solid fa-cloud-hail-mixed" /> -->
         <figure>
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" />
+          <img
+            :src="`http://openweathermap.org/img/wn/${weather.current.icon}@2x.png`"
+          />
         </figure>
         <div class="card-body">
           <h2 class="font-bold text-primary-focus">Montag, 21.05.2022</h2>
-          <p class="mt-2 text-primary-focus text-2xl">3 / 6°C</p>
-          <p class="mt-2 text-primary-focus">leichter Regen</p>
+          <p class="mt-2 text-primary-focus text-2xl">
+            {{ weather.current.tempMin }} / {{ weather.current.tempMax }}°C
+          </p>
+          <p class="mt-2 text-primary-focus">
+            {{ weather.current.description }}
+          </p>
         </div>
       </div>
       <!-- <div class="grid grid-cols-5 mt-5">
