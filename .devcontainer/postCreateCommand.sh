960d8b0d956912d2e142c8fea694f1a2dc595868
Lugg <57572111+lucaslootalot@users.noreply.github.com>
2022-05-10 08:41:06 +0000

    Merge from "dev"

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,19 +1,18 @@
 #! /bin/bash
 
 #copy vs code config to root
-cp -r /home/docker/workspace/src/.devcontainer/.vscode/* /home/docker/workspace/src/.vscode/
-
+cp -r /PlantMap/src/.devcontainer/.vscode/ /PlantMap/.vscode/
 #install pre-commit in the git repo
-mkdir -p build/gRPC
-cd /home/docker/workspace/src || exit 1
 pre-commit install --install-hooks
 
 #generate python classes from protobuf messages
+mkdir -p build/gRPC
 python3 -m grpc_tools.protoc \
         -I=protobuf-msgs \
         --python_out=build/gRPC  \
         --grpc_python_out=build/gRPC \
         protobuf-msgs/*
 
-cd frontend || exit 1
-npm install
+# use npm ci to install all exact verion dependencies from package-lock.json
+cd vue  || exit 1
+npm ci
---
+++ vue/src/components/SidebarMenu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-content">
+      <nav class="bg-primary sidebar-nav">
+        <div class="mt-10 mb-10">
+          <router-link :to="'/dashboard'">
+            <img
+              src="@/assets/img/logo_small_white.svg"
+              class="w-10 h-10 mb-3 mx-auto"
+            />
+            <!-- ToDo: Create Image Component & custom class for this -->
+          </router-link>
+          <div class="mt-10">
+            <ul>
+              <!-- ToDo: Edit icons & links -->
+              <li class="my-8 text-center text-sm text-white">
+                <router-link to="/dashboard">
+                  <img
+                    src="@/assets/icons/dashboard.svg"
+                    class="w-6 h-6 mx-auto mb-1"
+                  />
+                  <p>Dashboard</p>
+                </router-link>
+              </li>
+              <li class="my-8 text-center text-sm text-white">
+                <router-link to="/crops">
+                  <img
+                    src="@/assets/icons/crops.svg"
+                    class="w-6 h-6 mx-auto mb-1"
+                  />
+                  <p>Crops</p>
+                </router-link>
+              </li>
+              <li class="my-8 text-center text-sm text-white">
+                <router-link to="/3d">
+                  <img
+                    src="@/assets/icons/3d.svg"
+                    class="w-6 h-6 mx-auto mb-1"
+                  />
+                  <p>3D</p>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- if the navbar has more items later, the profile item can be added to it-->
+        <div class="mb-10">
+          <router-link to="/profile">
+            <img src="@/assets/icons/user.svg" class="w-6 h-6 mx-auto" />
+          </router-link>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
