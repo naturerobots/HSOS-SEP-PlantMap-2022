052f4394529816d5ef02b33d4600fa1e79d0f2c7
Lugg <57572111+lucaslootalot@users.noreply.github.com>
2022-05-11 14:34:35 +0000

    feat: added first implementation of sensor comp

---
+++ vue/src/components/SensorComp.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card w-96 bg-light shadow-xl m-2">
+    <div class="card-body px-2 py-3">
+      <h2 class="text-base text-center text-primary-focus">Soil parameters</h2>
+      <div class="grid grid-cols-3 gap-3">
+        <sensor-status
+          sensorName="Sensor A"
+          moisture="100"
+          temp="20"
+          ec="400"
+        ></sensor-status>
+        <sensor-status
+          sensorName="Sensor B"
+          moisture="15"
+          temp="5"
+          ec="99"
+        ></sensor-status>
+        <sensor-status
+          sensorName="Sensor C"
+          moisture="34"
+          temp="-3"
+          ec="3"
+        ></sensor-status>
+        <sensor-status
+          sensorName="Sensor D"
+          moisture="5"
+          temp="100"
+          ec="122"
+        ></sensor-status>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SensorStatus from "@/components/SensorStatus.vue";
+
+/*
+//icons for testing
+const upGreen = "up_green";
+const downGreen = "down_green";
+const upRed = "up_red";
+const downRed = "down_red";
+*/
+</script>
