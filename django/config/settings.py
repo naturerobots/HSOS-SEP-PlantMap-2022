c0d90d2e92e5fe555ee140e1f424591e8d33aa53
Julian Arkenau <arkenaujulian@gmail.com>
2022-05-11 08:31:24 +0000

    build: connect django to postgreSQL

--- django/config/settings.py
+++ django/config/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
