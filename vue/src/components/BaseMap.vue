86992aefd7b11fa4ed3b25cfd1fd12169247a0d6
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-06-01 21:32:19 +0000

    feat: created basemap comp

---
+++ vue/src/components/BaseMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card w-full h-full bg-white shadow-xl">
+    <div class="card-body p-0">
+      <leaflet-comp
+        :maxZoom="maxZoom"
+        :zoom="zoom"
+        :map-image="mapImage"
+      ></leaflet-comp>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LeafletComp from "@/components/leaflet/LeafletComp.vue";
+import type { MapImage } from "@/types/mapImage";
+
+defineProps({
+  maxZoom: {
+    type: Number,
+  },
+  zoom: {
+    type: Number,
+  },
+  mapImage: {
+    type: Object as () => MapImage,
+    required: true,
+  },
+});
+</script>
