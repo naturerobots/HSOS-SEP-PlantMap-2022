b0798df74fbaf5b0e57fe187297d25d41d3f48ea
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-06-03 16:44:22 +0000

    feat: added addMarker function from leaflet comp

--- vue/src/components/BaseMap.vue
+++ vue/src/components/BaseMap.vue
@@ -5,6 +5,7 @@
         :maxZoom="maxZoom"
         :zoom="zoom"
         :map-image="mapImage"
+        ref="leafletRef"
       ></leaflet-comp>
     </div>
   </div>
@@ -13,10 +14,22 @@
 <script setup lang="ts">
 import LeafletComp from "@/components/Leaflet/LeafletComp.vue";
 import type { MapImage } from "@/types/mapImage";
+import { ref } from "vue";
+import type L from "leaflet";
+
+const leafletRef = ref<InstanceType<typeof LeafletComp> | null>(null);
 
 defineProps<{
   maxZoom?: number;
   zoom?: number;
   mapImage: MapImage;
 }>();
+
+defineExpose({
+  addMarker,
+});
+
+function addMarker(marker: L.Marker): void {
+  leafletRef.value?.addMarker(marker);
+}
 </script>
