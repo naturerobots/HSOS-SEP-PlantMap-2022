75f05d73f2d86ba6f2ddbad0da178a934af79b7f
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-06-03 18:30:54 +0000

    feat: added polygon with hover for testing

--- vue/src/components/CropsMap.vue
+++ vue/src/components/CropsMap.vue
@@ -1,11 +1,15 @@
+<!--everything in this comp is for testing only-->
 <template>
-  <base-map :map-image="mapImage" :zoom="20.5"></base-map>
+  <base-map :map-image="mapImage" :zoom="20.5" ref="baseMapRef"></base-map>
 </template>
 
 <script setup lang="ts">
 import BaseMap from "@/components/BaseMap.vue";
 import type { MapImage } from "@/types/mapImage";
-import { LatLng } from "leaflet";
+import L, { LatLng } from "leaflet";
+import { ref, onMounted } from "vue";
+
+const baseMapRef = ref<InstanceType<typeof BaseMap> | null>(null);
 
 const mapImage: MapImage = {
   src: "https://cloud.naturerobots.de/apps/files_sharing/publicpreview/xZj9ytRt8WKr5cw?file=/goeoentueuegs_ibbenbueren_new2.jpg&fileId=28565&x=1920&y=1080&a=true",
@@ -13,4 +17,28 @@ const mapImage: MapImage = {
   top_right: new LatLng(52.316885520295315, 7.630280744986272),
   bottom_left: new LatLng(52.31721038577891, 7.630586922168733),
 };
+
+onMounted(() => {
+  let latlng: L.LatLng[] = [];
+  latlng.push(new LatLng(52.317039789516954, 7.630354102703116));
+  latlng.push(new LatLng(52.316941166226066, 7.630439482077206));
+  latlng.push(new LatLng(52.31693439650253, 7.630420101787746));
+  latlng.push(new LatLng(52.317031328642415, 7.630334734025543));
+  addBedBorder(latlng);
+});
+
+function addBedBorder(latlng: L.LatLng[]) {
+  let polygon = L.polygon(latlng, { color: "green", fillOpacity: 0 });
+  polygon.on("mouseover", function () {
+    polygon.setStyle({
+      fillOpacity: 0.2,
+    });
+  });
+  polygon.on("mouseout", function () {
+    polygon.setStyle({
+      fillOpacity: 0,
+    });
+  });
+  baseMapRef.value?.addPolygon(polygon);
+}
 </script>
