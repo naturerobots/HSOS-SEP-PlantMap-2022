2045ea899a982398fc3decde2df7740580735854
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-06-06 14:32:25 +0000

    feat: changed marker, added sensor events

--- vue/src/components/GardenMap.vue
+++ vue/src/components/GardenMap.vue
@@ -1,6 +1,13 @@
 <!--everything in this comp is for testing only-->
 <template>
-  <base-map :map-image="mapImage" :zoom="20.5" ref="baseMapRef"></base-map>
+  <base-map
+    :map-image="mapImage"
+    :zoom="20.5"
+    ref="baseMapRef"
+    @marker-click="markerClick"
+    @marker-enter="markerEnter"
+    @marker-leave="markerLeave"
+  ></base-map>
 </template>
 
 <script setup lang="ts">
@@ -8,8 +15,20 @@ import BaseMap from "@/components/BaseMap.vue";
 import type { MapImage } from "@/types/mapImage";
 import L, { LatLng } from "leaflet";
 import { ref, onMounted } from "vue";
+import type { Sensor } from "@/types/sensor";
 
 const baseMapRef = ref<InstanceType<typeof BaseMap> | null>(null);
+const markerMap: Map<number, L.Marker> = new Map();
+
+const props = defineProps<{
+  sensors: Sensor[];
+}>();
+
+const emit = defineEmits<{
+  (event: "sensorClick", sensorId: number): void;
+  (event: "sensorEnter", sensorId: number): void;
+  (event: "sensorLeave", sensorId: number): void;
+}>();
 
 const mapImage: MapImage = {
   src: "https://cloud.naturerobots.de/apps/files_sharing/publicpreview/xZj9ytRt8WKr5cw?file=/goeoentueuegs_ibbenbueren_new2.jpg&fileId=28565&x=1920&y=1080&a=true",
@@ -18,26 +37,67 @@ const mapImage: MapImage = {
   bottom_left: new LatLng(52.31721038577891, 7.630586922168733),
 };
 
-const sensorIcon: L.Icon = new L.Icon({
-  iconUrl: "src/assets/marker/sensor_icon.svg",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
 onMounted(() => {
-  addSensorMarker(new LatLng(52.317033021766406, 7.630251717595675));
-  addSensorMarker(new LatLng(52.317114252857905, 7.630492335061585));
-  addSensorMarker(new LatLng(52.31696187881402, 7.630343068498895));
-  addSensorMarker(new LatLng(52.31704148238147, 7.630583773056744));
+  props.sensors.forEach(function (sensor) {
+    addSensorMarker(sensor);
+  });
 });
 
-function addSensorMarker(latlng: L.LatLng) {
-  let marker = L.marker(latlng, {
-    icon: sensorIcon,
+function addSensorMarker(sensor: Sensor): void {
+  let marker = L.marker(new LatLng(sensor.lat, sensor.lng), {
+    icon: L.divIcon({
+      html: '<div class="sensor-icon shadow-xl">' + sensor.name + "</div>",
+    }),
   });
+  markerMap.set(sensor.id, marker);
   baseMapRef.value?.addMarker(marker);
 }
+
+function getIdByMarker(marker: L.Marker): number {
+  for (let [key, value] of markerMap.entries()) {
+    if (value === marker) return key;
+  }
+  return -1;
+}
+
+function markerClick(marker: L.Marker): void {
+  const sensorId: number = getIdByMarker(marker);
+  if (sensorId != -1) {
+    emit("sensorClick", sensorId);
+  }
+}
+
+function markerEnter(marker: L.Marker): void {
+  const sensorId: number = getIdByMarker(marker);
+  if (sensorId != -1) {
+    emit("sensorEnter", sensorId);
+  }
+}
+
+function markerLeave(marker: L.Marker): void {
+  const sensorId: number = getIdByMarker(marker);
+  if (sensorId != -1) {
+    emit("sensorLeave", sensorId);
+  }
+}
 </script>
+
+<style>
+/*TODO: move to base.css */
+.sensor-icon {
+  width: 25px;
+  height: 25px;
+  line-height: 25px;
+  border-radius: 50%;
+  font-size: 10px;
+  color: #000;
+  text-align: center;
+  background: #fff;
+  margin-left: -12.5px;
+  margin-top: -12.5px;
+}
+
+.sensor-icon:hover {
+  background: #addb73;
+}
+</style>
