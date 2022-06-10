f4338a3534020f96e28cc257ebfb6fb8a279c782
frederik <frederik.marahrens@hs-osnabrueck.de>
2022-06-10 21:06:07 +0000

    feat: row-hover adjustments

--- vue/src/components/CropsTable.vue
+++ vue/src/components/CropsTable.vue
@@ -14,6 +14,96 @@
         :visible-columns="visCols"
         :pagination="pagination()"
       >
+        <template #body="props">
+          <q-tr no-hover :props="props">
+            <q-td key="id" :props="props">
+              {{ props.row.id }}
+            </q-td>
+
+            <q-td key="plant" :props="props">
+              {{ props.row.plant }}
+            </q-td>
+
+            <q-td key="variety" :props="props">
+              {{ props.row.variety }}
+            </q-td>
+
+            <q-td key="location" :props="props">
+              {{ props.row.location }}
+            </q-td>
+
+            <q-td key="soilHumidity" :props="props">
+              {{ props.row.soilHumidity }}
+            </q-td>
+
+            <q-td key="health" :props="props">
+              {{ props.row.health }}
+            </q-td>
+
+            <q-td key="status" :props="props">
+              {{ props.row.status }}
+            </q-td>
+
+            <q-td key="harvest" :props="props">
+              {{ props.row.harvest }}
+            </q-td>
+
+            <q-td key="yield" :props="props">
+              {{ props.row.yield }}
+            </q-td>
+
+            <q-td :props="props" key="3d">
+              <div>
+                <router-link to="/3d">
+                  <svg
+                    class="fill-black crops3d-icon-active mx-auto"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20.377"
+                    height="21.432"
+                    viewBox="0 0 30.377 31.432"
+                  >
+                    <g
+                      id="Gruppe_143"
+                      data-name="Gruppe 143"
+                      transform="translate(-53.469 -373.116)"
+                    >
+                      <path
+                        id="Pfad_148"
+                        data-name="Pfad 148"
+                        d="M4.838,0H1.026A.953.953,0,0,0,.073.953V4.829a.953.953,0,1,0,1.906,0V1.906H4.838A.953.953,0,1,0,4.838,0Z"
+                        transform="translate(53.396 373.116)"
+                      />
+                      <path
+                        id="Pfad_149"
+                        data-name="Pfad 149"
+                        d="M426.718,0h-3.812a.953.953,0,1,0,0,1.906h2.859V4.829a.953.953,0,0,0,1.906,0V.953A.953.953,0,0,0,426.718,0Z"
+                        transform="translate(-343.826 373.116)"
+                      />
+                      <path
+                        id="Pfad_150"
+                        data-name="Pfad 150"
+                        d="M4.837,424.757H1.978v-2.923a.953.953,0,1,0-1.906,0v3.876a.953.953,0,0,0,.953.953H4.837a.953.953,0,1,0,0-1.906Z"
+                        transform="translate(53.397 -22.124)"
+                      />
+                      <path
+                        id="Pfad_151"
+                        data-name="Pfad 151"
+                        d="M426.718,420.881a.953.953,0,0,0-.953.953v2.923h-2.859a.953.953,0,1,0,0,1.906h3.812a.953.953,0,0,0,.953-.953v-3.876A.953.953,0,0,0,426.718,420.881Z"
+                        transform="translate(-343.826 -22.124)"
+                      />
+                      <path
+                        id="Pfad_152"
+                        data-name="Pfad 152"
+                        d="M57.928,7.553,45.66.125a.848.848,0,0,0-.883,0L32.5,7.553a.932.932,0,0,0-.439.8s0,0,0,.005V23.083a.931.931,0,0,0,.448.8l12.265,7.423a.848.848,0,0,0,.883,0l12.26-7.423.006,0a.881.881,0,0,0,.442-.787s0-.007,0-.011V8.354s0,0,0-.005A.931.931,0,0,0,57.928,7.553ZM44.337,28.914l-9.624-5.827,7.419-4.446a.943.943,0,0,0,.326-1.257.861.861,0,0,0-1.2-.341l-7.431,4.45V9.94l10.512,6.309Zm.881-14.26L34.712,8.346,45.218,1.985,55.724,8.346Zm11.394,6.839-7.432-4.45a.861.861,0,0,0-1.2.341.943.943,0,0,0,.326,1.257l7.419,4.446L46.1,28.914V16.249L56.612,9.94V21.494Z"
+                        transform="translate(23.439 373.116)"
+                      />
+                    </g>
+                  </svg>
+                </router-link>
+              </div>
+            </q-td>
+          </q-tr>
+        </template>
         <template v-slot:top-right>
           <q-select
             v-model="visCols"
@@ -51,12 +141,12 @@
           </q-td>
         </template>
 
-        <template v-slot:body-cell-3d="props">
+        <!-- <template v-slot:body-cell-3d="props">
           <q-td :props="props">
             <div>
               <router-link to="/3d">
                 <svg
-                  class="fill-black hover-icon mx-auto"
+                  class="fill-black crops3d-icon-active mx-auto"
                   xmlns="http://www.w3.org/2000/svg"
                   width="20.377"
                   height="21.432"
@@ -102,7 +192,7 @@
               </router-link>
             </div>
           </q-td>
-        </template>
+        </template> -->
       </q-table>
     </div>
   </div>
