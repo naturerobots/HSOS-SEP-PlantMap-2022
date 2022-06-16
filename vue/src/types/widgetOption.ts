fcf3ccbe6c7e774a02975f56ea1ef0eecd65859f
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-06-16 16:14:03 +0000

    feat: created const widgetOptions

--- vue/src/types/widgetOption.ts
+++ vue/src/types/widgetOption.ts
@@ -1,9 +1,45 @@
 export interface WidgetOption {
-  name: string;
   label: string;
+  name: string;
 }
-export type StoreOption =
-  | "weather"
-  | "soil-parameter"
-  | "notifications"
-  | "garden";
+
+export const widgetOptions = {
+  weather: {
+    label: "weather",
+    name: "weather",
+  },
+  soilParameter: {
+    label: "soil parameter",
+    name: "soil-parameter",
+  },
+  notification: {
+    label: "notifications",
+    name: "notifications",
+  },
+  gardenMap: {
+    label: "garden map",
+    name: "garden-map",
+  },
+  cropsTable: {
+    label: "crops table",
+    name: "crops-table",
+  },
+  cropsMap: {
+    label: "crops map",
+    name: "crops-map",
+  },
+  "3dTable": {
+    label: "3D table",
+    name: "3d-table",
+  },
+  "3dMap": {
+    label: "3D map",
+    name: "3d-map",
+  },
+} as const;
+
+//type of widgetOptions keys
+type widgetOptionsKeys = keyof typeof widgetOptions;
+
+//type of widgetOptions names
+export type StoreOption = typeof widgetOptions[widgetOptionsKeys]["name"];
