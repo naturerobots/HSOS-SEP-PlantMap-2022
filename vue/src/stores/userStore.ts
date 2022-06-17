5aef36f970d6be3900e5b1c32a63b0e0e118b778
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-06-17 09:20:36 +0000

    feat: added user to store + add/remove functions

--- vue/src/stores/userStore.ts
+++ vue/src/stores/userStore.ts
@@ -1,30 +1,46 @@
 import { defineStore } from "pinia";
+import type { User } from "@/types/user";
 import type { StoreOption } from "@/types/widgetOption";
 
 // TODO: move to types/store
 interface userStore {
-  options: StoreOption[];
+  user: User;
 }
 
 export const userStore = defineStore({
   id: "userStore",
   state: (): userStore => ({
-    options: [
-      "weather",
-      "garden-map",
-      "soil-parameter",
-      "crops-table",
-      "crops-map",
-      "3d-table",
-      "3d-map",
-    ], //TODO: delete later
+    user: {
+      settings: {
+        widgetOptions: [
+          "weather",
+          "garden-map",
+          "soil-parameter",
+          "crops-table",
+          "crops-map",
+          "3d-table",
+          "3d-map",
+        ], //TODO: delete later
+      },
+    },
   }),
   getters: {
     getOptions(state: userStore): StoreOption[] {
-      return state.options;
+      return state.user.settings.widgetOptions;
     },
   },
   actions: {
+    addOption(storeOption: StoreOption): void {
+      this.user.settings.widgetOptions.push(storeOption);
+      this.updateOptions();
+    },
+    removeOption(storeOption: StoreOption): void {
+      const index = this.user.settings.widgetOptions.indexOf(storeOption, 0);
+      if (index > -1) {
+        this.user.settings.widgetOptions.splice(index, 1);
+      }
+      this.updateOptions();
+    },
     async updateOptions(): Promise<void> {
       //TODO: send options to API
     },
