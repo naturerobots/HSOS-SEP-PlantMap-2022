9e11dc159e3181789768f870d1f34f3fc622a66a
lsiebels <lucas.siebels@hs-osnabrueck.de>
2022-06-17 11:45:39 +0000

    fix: headerbar when no options

--- vue/src/views/CropImageView.vue
+++ vue/src/views/CropImageView.vue
@@ -1,11 +1,11 @@
 <template>
-  <header-bar title="Crop image"></header-bar>
+  <header-bar title="Crop Image"></header-bar>
   <!-- <image-crop></image-crop> -->
   <image-crop-quasar></image-crop-quasar>
 </template>
 
 <script setup lang="ts">
-import HeaderBar from "@/components/HeaderBar.vue";
-import ImageCrop from "@/components/ImageCrop.vue";
+import HeaderBar from "@/components/header/HeaderBar.vue";
+//import ImageCrop from "@/components/ImageCrop.vue";
 import ImageCropQuasar from "@/components/ImageCropQuasar.vue";
 </script>
