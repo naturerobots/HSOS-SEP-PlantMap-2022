1b538cbc1ad45bc36479d04d9081d05cee404c14
Timo Dobrowolski <timo.dobrowolski@hs-osnabrueck.de>
2022-06-22 13:16:15 +0000

    feat: created register comp and view

---
+++ vue/src/views/RegisterView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <RegisterForm />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import RegisterForm from "@/components/register/RegisterForm.vue";
+</script>
