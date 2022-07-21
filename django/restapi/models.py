d06024faf36d8537d8c955330c379d29129497d8
Lukas Niehaus <lniehaus92@gmail.com>
2022-07-21 18:54:16 +0000

    fix: makemigrations by removing wrapper from path_and_rename

--- django/restapi/models.py
+++ django/restapi/models.py
@@ -53,12 +53,8 @@ class CompanySerializer(serializers.ModelSerializer):
 
 # Creates random filenames for garden images
 def path_and_rename(path):
-    def wrapper(instance, filename):
-        ext = filename.split('.')[-1]
-        filename = '{}.{}'.format(uuid4().hex, ext)
-        return os.path.join(path, filename)
-
-    return wrapper
+    filename = '{}.{}'.format(uuid4().hex, 'png')
+    return os.path.join(path, filename)
 
 
 class Garden(models.Model):
