3bf6933b8ff01df084d9ecac1fe8c7c56b494096
Lukas Niehaus <lniehaus92@gmail.com>
2022-08-17 13:43:12 +0000

    feat: use bed id in request

--- django/restapi/views/three_scene.py
+++ django/restapi/views/three_scene.py
@@ -1,13 +1,12 @@
-import base64
-import re
+import logging
 from os import listdir
 from os.path import isfile, join
 
 import numpy as np
 from conversions.transform_coordinates import plantLocationOffset
-from plyfile import PlyData
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from restapi.models import Bed, Company, Garden
 
 '''
 from restapi.helpers.auth import (
@@ -23,14 +22,32 @@ from restapi.models import Garden
 
 from django.http import *
 
+logger = logging.getLogger('django')
 
 # @permission_classes([AllowAny])
 @api_view(['GET', 'POST'])
 def get3dPlants(request, company_id: int, garden_id: int, bed_id: int):
 
+    '''
+    if (
+        not isGardenUser(garden_id, request.user.username)
+        and not isGardenAdmin(garden_id, request.user.username)
+        and not isCompanyUser(company_id, request.user.username)
+        and not isCompanyAdmin(company_id, request.user.username)
+    ):
+        return HttpResponseForbidden()
+    '''
+
     # Get bed Hash
 
-    projectUUID = 'e1ef73b1258b475a996d2b72924c27ac'
+    company = Company.objects.get(id=company_id)
+    garden = Garden.objects.get(id=garden_id, company=company)
+    bed = Bed.objects.get(id=bed_id, garden=garden)
+    # projectUUID = 'e1ef73b1258b475a996d2b72924c27ac'
+    projectUUID = str(bed)
+
+    # logger.info(f"bedData: {bed}")
+
     localPath = 'storage/media/pointclouds/ply/' + str(projectUUID) + '/'
     externalPath = 'media/pointclouds/ply/' + str(projectUUID) + '/'
     url = "http://localhost:8000/" + externalPath
@@ -46,28 +63,6 @@ def get3dPlants(request, company_id: int, garden_id: int, bed_id: int):
     for file in onlyfiles:
         geometryUUID = file[:-4]
         locationOffset = plantLocationOffset(projectUUID=projectUUID, geometryUUID=geometryUUID, dimensions=3)
-        '''
-        plant = {
-            "geometryUUID": geometryUUID,
-            "url": url + file,
-            "name": "Swiss Chard",
-            "locationDescription": "Row 12",
-            "location": {
-                "x": str(locationOffset[0]),
-                "y": str(locationOffset[1]),
-                "z": str(locationOffset[2]),
-            },
-            "health": {
-                "disease": 1,
-                "nutrient": 2,
-                "watering": 3,
-            },
-            "yield": "N/A",
-            "status": "Vegetating",
-            "Harvest": "2 Weeks",
-            "progress": 0.6,
-        }
-        '''
 
         totalX.append(locationOffset[0])
         totalY.append(locationOffset[1])
